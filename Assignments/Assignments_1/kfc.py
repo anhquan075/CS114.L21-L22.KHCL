c4d9a02be0e49827a5c1964871f71a70373a93df
anhquan075 <19522081@gm.uit.edu.vn>
2021-03-05 13:20:44 +0000

    Add py file to assigments 1 folder

---
+++ Assignments/Assignments_1/kfc.py
@@ -0,0 +1,9 @@
+from decimal import *
+
+F = float(input())
+C = (F - 32) / 1.8
+K = (F - 32) / 1.8 + 273.15
+getcontext().prec = 6
+print(Decimal(C).normalize(),Decimal(K).normalize())
+
+
---
+++ Assignments/Assignments_1/pressure.py
@@ -0,0 +1,10 @@
+from decimal import *
+
+psi = float(input())
+
+kgscm =  0.453592 / 2.54**2 * psi
+
+getcontext().prec = 6
+
+print(Decimal(kgscm).normalize())
+
