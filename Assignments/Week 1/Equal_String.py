6f963b385755094d2aeeead60cdfb775258c9faa
anhquan075 <19522081@gm.uit.edu.vn>
2021-03-18 06:56:26 +0000

    Add to revert 3

--- Assignments/Week 1/Equal_String.py
+++ Assignments/Week 1/Equal_String.py
@@ -1,19 +1,16 @@
-q = int(input())
+n = int(input())
 result = []
 
-for k in range(q):
+for k in range(n):
   s = input()
   t = input()
   flag = False
-
 
   for i in s:
     if flag == False:
       for j in t:
         if i == j:
           flag = True
-          
-
     else:
       break
 
--- Assignments/Week 1/LanLanguage.py
+++ Assignments/Week 1/LanLanguage.py
@@ -31,7 +31,6 @@ for i in range(len(t)):
 x=sorted(t)
 cnt=0
 
-
 for i in range(len(t)):
     if t[i]==3 or t[i]==4:
         cnt+=1
